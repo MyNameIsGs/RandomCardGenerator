95f68c705a50a3edc6ef0a4dcaf4e148599f2479
MyNameIsGs <sebascarmun@gmail.com>
2023-01-16 21:53:14 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const cards = ["♦", "♥", "♠", "♣"];
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const topElement = document.querySelector(".top");
+  const numberElement = document.querySelector(".number");
+  const footerElement = document.querySelector(".footer");
+
+  const singleCard = cards[Math.floor(Math.random() * cards.length)];
+  const singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+  if (singleCard === "♥" || singleCard === "♦") {
+    topElement.style.color = "red";
+    footerElement.style.color = "red";
+  }
+  topElement.innerHTML = singleCard;
+  numberElement.innerHTML = singleNumber;
+  footerElement.innerHTML = singleCard;
+
+  document.querySelector(".card").classList.add();
 };
